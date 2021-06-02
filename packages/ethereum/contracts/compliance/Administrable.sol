aa1868ceebd84341c9bd046b564f29f813237e55
Karim Kawambwa <4308339+karimkawambwa@users.noreply.github.com>
2021-06-02 15:06:21 -0230

    split roles to a separate file

--- packages/ethereum/contracts/compliance/Administrable.sol
+++ packages/ethereum/contracts/compliance/Administrable.sol
@@ -2,28 +2,11 @@
 
 pragma solidity >=0.6.0 <0.8.0;
 
+import "./Roles.sol";
 import "../interface/IERC1400Validator.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
-abstract contract Administrable is AccessControl {
-    bytes32 public constant ADMIN_ROLE = DEFAULT_ADMIN_ROLE;
-
-    // keccak256("CONTROLLER_ROLE")
-    bytes32 public constant CONTROLLER_ROLE =
-        0x7b765e0e932d348852a6f810bfa1ab891e259123f02db8cdcde614c570223357;
-
-    // keccak256("BURNER_ROLE")
-    bytes32 public constant BURNER_ROLE =
-        0x3c11d16cbaffd01df69ce1c404f6340ee057498f5f00246190ea54220576a848;
-
-    // keccak256("MINTER_ROLE")
-    bytes32 public constant MINTER_ROLE =
-        0x9f2df0fed2c77648de5860a4cc508cd0818c85b8b8a1ab4ceeef8d981c8956a6;
-
-    // keccak256("PAUSER_ROLE")
-    bytes32 public constant PAUSER_ROLE =
-        0x65d7a28e3265b37a6474929f336521b332c1681b933f6cb9f3376673440d862a;
-
+abstract contract Administrable is AccessControl, Roles {
     modifier onlyAdmin {
         require(
             hasRole(ADMIN_ROLE, msg.sender),
---
+++ packages/ethereum/contracts/compliance/Roles.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.6.0 <0.8.0;
+
+abstract contract Roles {
+    // AccessControl DEFAULT_ADMIN_ROLE = 0x00
+    bytes32 public constant ADMIN_ROLE = 0x00;
+
+    // keccak256("CONTROLLER_ROLE")
+    bytes32 public constant CONTROLLER_ROLE =
+        0x7b765e0e932d348852a6f810bfa1ab891e259123f02db8cdcde614c570223357;
+
+    // keccak256("BURNER_ROLE")
+    bytes32 public constant BURNER_ROLE =
+        0x3c11d16cbaffd01df69ce1c404f6340ee057498f5f00246190ea54220576a848;
+
+    // keccak256("MINTER_ROLE")
+    bytes32 public constant MINTER_ROLE =
+        0x9f2df0fed2c77648de5860a4cc508cd0818c85b8b8a1ab4ceeef8d981c8956a6;
+
+    // keccak256("PAUSER_ROLE")
+    bytes32 public constant PAUSER_ROLE =
+        0x65d7a28e3265b37a6474929f336521b332c1681b933f6cb9f3376673440d862a;
+}
