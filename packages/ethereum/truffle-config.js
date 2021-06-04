d419bdbca88bef267c904708fcd6c8da5e3fd55a
Karim Kawambwa <4308339+karimkawambwa@users.noreply.github.com>
2021-06-04 17:44:21 -0230

    store network deployments

--- packages/ethereum/truffle-config.js
+++ packages/ethereum/truffle-config.js
@@ -3,7 +3,7 @@ const HDWalletProvider = require("@truffle/hdwallet-provider");
 
 module.exports = {
   contracts_directory: "./contracts",
-  contracts_build_directory: "./build",
+  contracts_build_directory: "./build/contracts",
   /**
    * Networks define how you connect to your ethereum client and let you set the
    * defaults web3 uses to send transactions. If you don't specify one truffle
