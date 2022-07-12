6860a836ecf794517e46c21b2eb3d3cb0b786908
Karim Kawambwa <4308339+karimkawambwa@users.noreply.github.com>
2022-07-12 13:01:53 -0230

    feat: pass all flags and arguments to the shell scripts

--- packages/tezos/compile.sh
+++ packages/tezos/compile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-~/smartpy-cli/SmartPy.sh compile $(PWD)/contracts/Migrations.py $(PWD)/build/migrations --purge
-~/smartpy-cli/SmartPy.sh compile $(PWD)/contracts/compliance/Whitelist.py $(PWD)/build/compliance --purge
-~/smartpy-cli/SmartPy.sh compile $(PWD)/contracts/extension/WhitelistValidator.py $(PWD)/build/extension --purge
-~/smartpy-cli/SmartPy.sh compile $(PWD)/contracts/token/FA1.2.py $(PWD)/build/token --purge
-~/smartpy-cli/SmartPy.sh compile $(PWD)/contracts/wallet/VestingEscrowMinterBurnerWallet.py $(PWD)/build/wallet --purge
+~/smartpy-cli/SmartPy.sh compile $(PWD)/contracts/Migrations.py $(PWD)/build/migrations --purge $@
+~/smartpy-cli/SmartPy.sh compile $(PWD)/contracts/compliance/Whitelist.py $(PWD)/build/compliance --purge $@
+~/smartpy-cli/SmartPy.sh compile $(PWD)/contracts/extension/WhitelistValidator.py $(PWD)/build/extension --purge $@
+~/smartpy-cli/SmartPy.sh compile $(PWD)/contracts/token/FA1.2.py $(PWD)/build/token --purge $@
+~/smartpy-cli/SmartPy.sh compile $(PWD)/contracts/wallet/VestingEscrowMinterBurnerWallet.py $(PWD)/build/wallet --purge $@
